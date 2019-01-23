a9025c449cd9f06db4c3f4d545b48b00bac552f4
Pacome BERNIER <pbernier@e3r8p13.42.fr>
2019-01-23 15:50:33 +0100

    [pbernier] - Starting main

--- includes/computorV1.h
+++ includes/computorV1.h
@@ -13,4 +13,25 @@
 #ifndef COMPUTORV1_H
 # define COMPUTORV1_H
 
+# include <unistd.h>
+
+typedef struct s_equation   t_equation;
+typedef struct s_computor   t_computor;
+typedef struct s_verbos     t_verbos;
+
+struct  s_computor
+{
+        size_t i;
+};
+
+struct  s_equation
+{
+        int tmp;
+};
+
+struct s_verbos
+{
+        int tmp;
+};
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -10,7 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int     main(void)
+#include <computorV1.h>
+
+int     main(int argc, char **argv)
 {
+    t_computor	computor;
+    t_equation  equation;
+    t_verbos    verbos;
+
+    computor.i = 0;
+    equation.tmp = 0;
+    verbos.tmp = 0;
+    (void)argc;
+    (void)argv;
+	//initialization
+    //verification entrer
+    //boucle verification caractere par caractere
+        //stokage des infos
+        //verirification verbos
+    //si pas erreur de verbos
+        //calcul puis imprim
+    //else
+        //imprime le verbos
     return (0);
 }
